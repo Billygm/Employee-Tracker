70c9d5c816fc3febad3baeab0e65ac8e95ef50c1
Billy Mott <billygm@uw.edu>
2022-02-03 13:17:03 -0800

    finished options function and view functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express = require('express');
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const db = require('./db/connection');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const db = require("./db/connection");
 
 // const PORT = process.env.PORT || 3001;
 // const app = express();
@@ -9,89 +9,112 @@ const db = require('./db/connection');
 // app.use(express.urlencoded({ extended: false }));
 // app.use(express.json());
 
-
 async function presentOptions() {
-  const answers = await inquirer.prompt([
+  const answer = await inquirer.prompt([
     {
-      type: 'list',
-      name: 'options',
-      message: 'What would you like to do?',
+      type: "list",
+      name: "options",
+      message: "What would you like to do?",
       choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees',
-        'Add a department',
-        'Add a role',
-        'Add an employee',
-        'Update an employee role'
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
       ],
     },
   ]);
+
+  if (answer.options === "View all departments") {
+    viewAllDepartments();
+  } else if (answer.options === "View all roles") {
+    viewAllRoles();
+  } else if (answer.options === "View all employees") {
+    viewAllEmployees();
+  } else if (answer.options === "Add a department") {
+    addDepartment();
+  } else if (answer.options === "Add a role") {
+    addRole();
+  } else if (answer.options === "Add an employee") {
+    addEmployee();
+  } else if (answer.options === "Update an employee role") {
+    updateEmployeeRole();
+  }
 }
 
 async function viewAllDepartments() {
-  const departments = await db.query('SELECT * FROM departments');
-  
+  const departments = await db.query("SELECT * FROM departments");
+
   console.table(departments);
+  presentOptions();
 }
 
 async function viewAllRoles() {
-  const roles = await db.query('SELECT * FROM roles');
-  
+  const roles = await db.query("SELECT * FROM roles");
+
   console.table(roles);
+  presentOptions();
 }
 
 async function viewAllEmployees() {
-  const employees = await db.query('SELECT * FROM employees');
-  
+  const employees = await db.query("SELECT * FROM employees");
+
   console.table(employees);
+  presentOptions();
 }
 
-async function createRole() {
-  
+async function addDepartment() {}
+
+async function addEmployee() {}
+
+async function addRole() {
   const departments = [
     {
       id: 1,
-      name: 'Sales'
+      name: "Sales",
     },
     {
       id: 2,
-      name: 'Finance'
+      name: "Finance",
     },
     {
       id: 3,
-      name: 'Engineering'
+      name: "Engineering",
     },
     {
       id: 4,
-      name: 'Leagal'
-    }
+      name: "Leagal",
+    },
   ];
-  
-  const choices = department.map(department => {
+
+  const choices = department.map((department) => {
     return {
       name: department.name,
-      value: department.id
-    }
+      value: department.id,
+    };
   });
-  
+
   const answers = await inquirer.prompt([
     {
-      type: 'list',
-      name: 'department_id',
-      message: 'choose a department',
+      type: "list",
+      name: "department_id",
+      message: "choose a department",
       choices: [
-        { name: 'Sales', value: 1 },
-        { name: 'Finance', value: 2 },
-        { name: 'Engineering', value: 3 },
-        { name: 'Leagal', value: 4 },
+        { name: "Sales", value: 1 },
+        { name: "Finance", value: 2 },
+        { name: "Engineering", value: 3 },
+        { name: "Leagal", value: 4 },
       ],
     },
   ]);
 }
 
-presentOptions()
+async function updateEmployeeRole(){}
+
+presentOptions();
 
 // app.listen(PORT, () => {
-  //     console.log(`Server running on port ${PORT}`);
-  //   });
+//     console.log(`Server running on port ${PORT}`);
+//   });
